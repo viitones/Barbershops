b059c6e41b8f5774c11d08f50bb0813c7a3e9671
viitones <vitonesmartins@gmail.com>
2025-08-21 22:58:14 -0300

    feat:  add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import { Header } from "./_components/header"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { Badge } from "./_components/ui/badge"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
 
 export default function Home() {
@@ -14,7 +17,7 @@ export default function Home() {
 
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca" />
-          <Button variant="outline">
+          <Button>
             <SearchIcon />
           </Button>
         </div>
@@ -27,6 +30,27 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://github.com/viitones.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">20</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
